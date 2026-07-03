d8ee005ececc76feca3fbcf036afd511e7b0891f
Prashant Singh <prashantsingh97393535@gmail.com>
2026-07-03 23:09:57 +0530

    final complete working version

--- server/routes/solvedProblemsRoutes.js
+++ server/routes/solvedProblemsRoutes.js
@@ -10,15 +10,6 @@ import deleteProblem from '../controllers/deleteProblemController.js';
 
 router.get('/', protect, getReviewedProblems);
 
-router.get("/test", (req, res) => {
-  res.json({ message: "Test route works" });
-});
-
-// router.delete("/:problemId", protect, deleteProblem);
-
-router.delete("/:problemId", protect , (req, res, next) => {
-    console.log("DELETE ROUTE HIT");
-    next();
-} , deleteProblem);
+router.delete("/:problemId", protect , deleteProblem);
 
 export default router;
